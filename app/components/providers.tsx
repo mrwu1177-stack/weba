18733e9572ab14d744b63d2db603f2f2d0df63d3
mrwu1177-stack <mrwu1177@gmail.com>
2026-02-01 10:14:59 +0800

    1014

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { getQueryClient } from './lib/react-query';
+import { getQueryClient } from '@/lib/react-query';
 
 export function Providers({ children }) {
   const queryClient = getQueryClient();
